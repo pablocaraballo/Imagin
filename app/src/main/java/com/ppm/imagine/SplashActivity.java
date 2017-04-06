d6f7088eb0a7761e80f7709c54a8f8c395813bb4
pablocaraballo <ppatinoc@elpuig.xeill.net>
2017-04-06 15:52:47 +0200

    Splash Activity

---
+++ app/src/main/java/com/ppm/imagine/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.ppm.imagine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by tarda on 06/04/17.
+ */
+
+public class SplashActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this SplashActivity-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
