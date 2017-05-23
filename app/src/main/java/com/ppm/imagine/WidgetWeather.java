3fa2bc6baf62bdcf6872db293f022ba664e966b9
Ejemplo <ejemplo@ejemplo.com>
2017-05-23 17:40:42 +0200

    Commit widget weather changes

---
+++ app/src/main/java/com/ppm/imagine/WidgetWeather.java
@@ -0,0 +1,29 @@
+package com.ppm.imagine;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by tarda on 16/05/17.
+ */
+
+public class WidgetWeather {
+
+public void upToFirebase(String pathImage,String city, Double temp){
+
+
+}
+
+
+}
