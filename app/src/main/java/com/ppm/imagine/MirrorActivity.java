10fa542271b96ad17f0048e6d9a2c8bcc17fefc7
Manel <maguayoj@elpuig.xeill.net>
2017-05-30 17:03:52 +0200

    WidgetTime Fixed and Other things solved

--- app/src/main/java/com/ppm/imagine/MirrorActivity.java
+++ app/src/main/java/com/ppm/imagine/MirrorActivity.java
@@ -8,7 +8,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.util.Log;
+import android.view.Display;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -119,6 +121,12 @@ public class MirrorActivity extends GoogleApiActivity {
 
     public void refreshListView(){
 
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        double ratio = ((float) (width))/300.0;
+        int height = (int)(ratio*50);
+
+
         layout.removeView(currentListview);
 
         System.out.println("WIDGETW DENTRO REFRESH");
@@ -145,7 +153,12 @@ public class MirrorActivity extends GoogleApiActivity {
             }
 
             currentListview=lv;
-            layout.addView(lv);
+
+
+
+            layout.addView(lv, new GridLayout.LayoutParams(
+                    GridLayout.spec(User.mirrors.get(Configurator.espejoActual).getConfigurator().getWidgetTwitter().getPosYinMirror(), GridLayout.CENTER),
+                    GridLayout.spec(User.mirrors.get(Configurator.espejoActual).getConfigurator().getWidgetTwitter().getPosXinMirror(), GridLayout.CENTER)));
         }
     }
 
